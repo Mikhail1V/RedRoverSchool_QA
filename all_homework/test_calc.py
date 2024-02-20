d6df294050ccc2e06e0077239a656ac2500c14c2
Mikhail1V <vmihailps@gmail.com>
2024-02-20 15:12:07 +0300

    add file hw_04

---
+++ all_homework/test_calc.py
@@ -0,0 +1,17 @@
+import my_calc
+
+# Сложение двух чисел
+result = my_calc.add(10, 5)
+print(result)
+
+# Вычитание двух чисел
+result = my_calc.subtract(10, 5)
+print(result)
+
+# Умножение двух чисел
+result = my_calc.multiply(10, 5)
+print(result)
+
+# Деление двух чисел
+result = my_calc.divide(10, 5)
+print(result)
